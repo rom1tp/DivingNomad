ae89003032480879f9bda5a040de31dfc9442a20
rom1tp <77027144+rom1tp@users.noreply.github.com>
2021-03-17 17:12:41 +0100

    post aside

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -4,6 +4,9 @@ class PostController extends FrontController
  public function display()
  {
   $post = $this->_postsModel->getPost($_GET["id"]);
+  $day = date("j", strtotime($post['date']));
+  $month = date("M", strtotime($post['date']));
+  $year = date("Y", strtotime($post['date']));
   $template = 'post.phtml';
   include $this->_layout;
  }
