f4489774741f598730fa1dd4f8184295287f53e1
rom1tp <77027144+rom1tp@users.noreply.github.com>
2021-03-25 16:23:39 +0100

    remote sql db

--- models/ModelManager.php
+++ models/ModelManager.php
@@ -6,15 +6,33 @@ class ModelManager
 
  public function __construct()
  {
+   // * LOCAL BLOG
+  //  $host = 'localhost';
+  //  $db = 'blog';
+  //  $user = 'root';
+  //  $pwd = '';
+  //  $charset = 'utf8';
+  //  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+   
+   // * IDE BLOG
+  //  $host = 'localhost';
+  //  $db = 'live-44_romaintan_blog';
+  //  $user = 'romaintan';
+  //  $pwd = '93edfa6fMjZjNzk4NDk5Y2NjYjRhNWRlNzg0Zjlk2712ff64';
+  //  $charset = 'utf8';
+  //  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+  // * DEPLOYMENT BLOG
+  $host = 'freedb.tech';
+  $db = 'freedbtech_divingnomad';
+  $user = 'freedbtech_Aarkans';
+  $pwd = 'DivingNomad17';
+  $charset = 'utf8';
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+  
   try
   {
-   // * LOCAL BLOG
-   $this->bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root','');
-
-   // * IDE BLOG
-   // $this -> bdd = new PDO('mysql:host=localhost;dbname=live-44_romaintan_blog;charset=utf8','romaintan','93edfa6fMjZjNzk4NDk5Y2NjYjRhNWRlNzg0Zjlk2712ff64');
-
-   // VOIR ERREURES MYSQL
+   $this->bdd = new PDO($dsn, $user, $pwd);
    $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
   } catch (Exception $e) {
    echo $e;
